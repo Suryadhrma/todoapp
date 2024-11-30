ff7cd15ea3b515d9d2f4f242b35728f011f72dc1
srya <m.surya.dharma9f@gmail.com>
2024-11-30 23:40:48 +0700

    segini dlu

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -4,104 +4,158 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // LOGO
-            Image.asset(
-              'assets/images/logoapp.png', // Path gambar logo
-              width: 150,
-              height: 150,
-            ),
-            const SizedBox(height: 40),
+      resizeToAvoidBottomInset: false, // Kembalikan ke false
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 90),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min, // Batasi ukuran kolom
+              children: [
+                // LOGO
+                    Image.asset(
+                      'assets/images/logoapp.png',
+                      width: 150,
+                      height: 150,
+                    ),
+                const SizedBox(height: 160),
 
-            // EMAIL FIELD
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Email', // Placeholder text
-                hintStyle: const TextStyle(fontFamily: 'Montserrat'),
-                filled: true, // Tambahkan warna background
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 16.0,
-                  horizontal: 12.0,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(
-                    color: Colors.grey, // Warna border saat tidak fokus
+                // EMAIL FIELD
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    hintStyle: const TextStyle(fontFamily: 'Montserrat'),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 12.0,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(
+                        color: Color(0xFFFF8C94),
+                        width: 2.0,
+                      ),
+                    ),
                   ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(
-                    color: Color(0xFFFF8C94), // Warna border saat fokus
-                    width: 2.0,
+                const SizedBox(height: 20),
+
+                // PASSWORD FIELD
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: const TextStyle(fontFamily: 'Montserrat'),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 12.0,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.visibility_off, color: Colors.grey),
+                      onPressed: () {
+                        // Aksi untuk toggle visibility password
+                      },
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: const BorderSide(
+                        color: Color(0xFFFF8C94),
+                        width: 2.0,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 20),
 
-            // PASSWORD FIELD
-            TextField(
-              obscureText: true, // Untuk menyembunyikan password
-              decoration: InputDecoration(
-                hintText: 'Password',
-                hintStyle: const TextStyle(fontFamily: 'Montserrat'),
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.symmetric(
-                  vertical: 16.0,
-                  horizontal: 12.0,
+                const SizedBox(height: 20),
+
+                // FORGOT PASSWORD BUTTON
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {
+                      // Aksi ketika tombol ditekan
+                    },
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(EdgeInsets.zero),
+                      minimumSize: MaterialStateProperty.all(Size.zero),
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      overlayColor: MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Color(0xFFFF8C94),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.visibility_off, color: Colors.grey),
+
+                const SizedBox(height: 20),
+
+                // SIGN IN BUTTON
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFFF8C94),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 105.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                // SIGNUP TEXT
+                TextButton(
                   onPressed: () {
-                    // Aksi untuk toggle visibility password
+                    // Navigator to signup page
                   },
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(
-                    color: Colors.grey,
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.zero),
+                    minimumSize: MaterialStateProperty.all(Size.zero),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    overlayColor: MaterialStateProperty.all(Colors.transparent),
+                  ),
+                  child: const Text(
+                    'Belum punya akun? Buat disini',
+                    style: TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Color.fromARGB(255, 80, 74, 74),
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: const BorderSide(
-                    color: Color(0xFFFF8C94),
-                    width: 2.0,
-                  ),
-                ),
-              ),
+              ],
             ),
-
-            const SizedBox(height: 30),
-
-            // SIGN IN BUTTON
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFFF8C94),
-                padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 200.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: const Text(
-                'SIGN IN',
-                style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 12.0,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
